0c03e70127c2fbfcd97fab7254ac3b0778fc0f25
bedibe <beadelino@gmail.com>
2022-11-03 10:42:09 -0300

    Rotas finalizadas
    API REST
    Json server
    Programadores Carioca
    Modulo 3
    Projeto individual

--- js/server.js
+++ js/server.js
@@ -7,4 +7,7 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+    //eslint-disable-next-line no console
+    console.log(`JSON Server está funcionando na porta ${port}`);
+});
