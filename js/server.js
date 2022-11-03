2682ee227e782a867d0e9a3babc996cd36829dbf
bedibe <beadelino@gmail.com>
2022-11-03 11:21:51 -0300

    modificando o js

--- js/server.js
+++ js/server.js
@@ -1,7 +1,8 @@
-import jsonServer from 'json-server';
+const jsonServer = require ('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
